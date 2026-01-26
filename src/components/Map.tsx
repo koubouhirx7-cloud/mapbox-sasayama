80ddf6296747e29972ffd151c61edd364f8b696b
koubouhirx7-cloud <info@high-lander2.com>
2026-01-26 16:01:18 +0900

    feat: enhance 3D terrain (exaggeration 2.2) and add sky atmosphere

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -208,14 +208,26 @@ const Map: React.FC<MapProps> = ({
                         });
                     }
 
-                    // 3. 3D Terrain
+                    // 3. 3D Terrain & Sky Atmosphere
                     map.addSource('mapbox-dem', {
                         'type': 'raster-dem',
                         'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
                         'tileSize': 512,
                         'maxzoom': 14
                     });
-                    map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+                    // Increase exaggeration to emphasize the "Basin" (Bonchi) feel
+                    map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2.2 });
+
+                    // Add Sky Layer for depth and atmosphere
+                    map.addLayer({
+                        'id': 'sky',
+                        'type': 'sky',
+                        'paint': {
+                            'sky-type': 'atmosphere',
+                            'sky-atmosphere-sun': [0.0, 0.0],
+                            'sky-atmosphere-sun-intensity': 15
+                        }
+                    });
                 });
 
                 mapRef.current.on('load', async () => {
